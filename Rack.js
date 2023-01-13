0b9f48bcd7c5f8613fb86d21c84349cf59926f20
Eve Crabb <eve@giffgaff.co.uk>
2023-01-13 14:55:45 +0000

    add first idea on solution

--- Rack.js
+++ Rack.js
@@ -13,11 +13,15 @@ class Rack {
   add(ball) {
     //add the ball into balls array in ascending order
     /*
-                []                 
+                []
         20 --> [20]
-        
 
-    
+        add(5) = [2, 3, 5]
+        for each in the current array, take away the new number from i, so
+        2 - 5 = -3,
+        3 - 5, -2.
+        The smallest result indicates the closest position?
+
     */
   }
 }
